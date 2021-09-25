d77c9f532b93c4676792c48037fac2d7919cae1f
Anthony <anthony.zamarro@gmail.com>
2021-09-25 18:25:10 -0400

    update mutations

--- lireddit-server/src/resolvers/user.ts
+++ lireddit-server/src/resolvers/user.ts
@@ -29,6 +29,56 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+    @Mutation(() =>  UserResponse)
+    async changePassword(
+        @Arg('token') token: string,
+        @Arg('newPassword') newPassword: string,
+        @Ctx() {redis, em, req}: MyContext
+    ): Promise<UserResponse> {
+        if (newPassword.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: 'newPassword',
+                        message: 'length must be greater than two.'
+                    }
+                ]
+            }
+        }
+
+        const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+
+        if (!userId) {
+            return {
+                errors: [{
+                    field: 'token',
+                    message: 'token expired' 
+                }]
+            };
+        }
+
+        const user = await em.findOne(User, {id: parseInt(userId) });
+
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: 'token',
+                        message: 'user no longer exists'
+                    }
+                ]
+            } 
+        }
+
+        user.password = await argon2.hash(newPassword);
+        await em.persistAndFlush(user);
+
+        // auto-login user after change password
+        req.session.userId = user.id;
+
+        return { user }
+    }
+
     @Mutation(() => Boolean)
     async forgotPassword(
         @Arg('email') email: string,
